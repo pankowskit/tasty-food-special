39b3177424b550fd82f136dfd4aa639861d216e6
tomaszpankowski <--get-all>
2022-04-23 00:13:25 +0200

    routing and tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Routes, {
+    AboutPage,
+    CartPage,
+    ErrorPage,
+    HomePage,
+    LocationPage,
+    PrivacyPage,
+    ProductsPage} from './App';
+import {Route} from "react-router-dom";
+import ComingsoonPage from "./views/comingsoon";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter: new Adapter()});
+
+describe('Router tests', () => {
+    let pathMap;
+    beforeAll(() => {
+        const component = shallow(<Routes/>);
+        pathMap = component.find(Route).reduce((pathMap, route) => {
+            const routeProps = route.props();
+            pathMap[routeProps.path] = routeProps.component;
+            return pathMap;
+        }, {});
+    });
+    it('Should show 404 error page for /xxx route', () => {
+        expect(pathMap['/xxx']).toBe(ErrorPage);
+    });
+    it('Should show about page for /about route', () => {
+        expect(pathMap['/about']).toBe(AboutPage);
+    });
+    it('Should show cart page for /cart route', () => {
+        expect(pathMap['/cart']).toBe(CartPage);
+    });
+    it('Should show home page for / route', () => {
+        expect(pathMap['/']).toBe(HomePage);
+    });
+    it('Should show location page for /location route', () => {
+        expect(pathMap['/location']).toBe(LocationPage);
+    });
+    it('Should show privacy page for /privacy route', () => {
+        expect(pathMap['/privacy']).toBe(PrivacyPage);
+    });
+    it('Should show products page for /products route', () => {
+        expect(pathMap['/products']).toBe(ProductsPage);
+    });
 });
